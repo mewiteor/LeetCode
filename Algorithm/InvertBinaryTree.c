c4ee932064932b1f71f06c4b0fd5c464fb69d615
mewiteor <mewiteor@hotmail.com>
2016-04-02 12:05:36 +0800

    Add a code

---
+++ Algorithm/InvertBinaryTree.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2016  Mewiteor <mewiteor@hotmail.com>
+ * 
+ * This program is free software: you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Invert a binary tree.
+ * 
+ *      4
+ *    /   \
+ *   2     7
+ *  / \   / \
+ * 1   3 6   9
+ * to
+ *      4
+ *    /   \
+ *   7     2
+ *  / \   / \
+ * 9   6 3   1
+ * Trivia:
+ * This problem was inspired by this original tweet by Max Howell:
+ * Google: 90% of our engineers use the software you wrote (Homebrew),
+ * but you can’t invert a binary tree on a whiteboard so fuck off.
+ */
+
+/*
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     struct TreeNode *left;
+ *     struct TreeNode *right;
+ * };
+ */
+struct TreeNode* invertTree(struct TreeNode* root) {
+    if(root) {
+        struct TreeNode* temp=root->left;
+        root->left=invertTree(root->right);
+        root->right=invertTree(temp);
+    }
+    return root;
+}
